49807ae1cd6890e2b113389317e4a2881fbafbe0
seoilgun <seoilgun@gmail.com>
2017-12-01 14:15:23 +0900

    test

--- model/deconv_conv.py
+++ model/deconv_conv.py
@@ -15,18 +15,20 @@ def build_model(inputs, num_classes, is_training, model_conf):
         net = tf.layers.conv2d_transpose(net, filter_size[i], filters[i], strides=strides[i],
                                          padding='valid',
                                          kernel_initializer=tf.variance_scaling_initializer(), name="deconv" + str(i))
+
         if model_conf.bn:
             net = tf.layers.batch_normalization(inputs=net, axis=3, momentum=bn_decay, epsilon=bn_epsilon,
                                                 center=True, scale=True, training=True, fused=True,
                                                 name="batch" + str(i))
-
-        net = tf.nn.relu(net, name="relu" + str(i))
+        # net = tf.nn.relu(net, name="relu" + str(i))
+        net = tf.nn.sigmoid(net, name="sigmoid" + str(i))
         if model_conf.add_image:
             if not model_conf.add_image_interval or (i != 0 and i % model_conf.add_image_interval == 1):
                 mid_image = tf.image.resize_images(inputs, [net.get_shape()[1], net.get_shape()[1]])
                 if mid_image.get_shape()[3] == 3:
                     mid_image = tf.reduce_mean(mid_image, 3, keep_dims=True)
                 net = tf.add(net, mid_image, name="mid_image_" + str(i))
+
     gen_image_size = net.get_shape()[1]
     gen_x_ = tf.image.resize_images(inputs, [gen_image_size, gen_image_size])
     gen_x_ = tf.reshape(gen_x_, [-1, gen_image_size * gen_image_size], name="gen_y_vectorize")
