d808f918cf356aac65e43221d4801af85e34a51e
seoilgun <seoilgun@gmail.com>
2017-12-31 01:52:35 +0900

    test

--- train_bulk.py
+++ train_bulk.py
@@ -185,8 +185,8 @@ DEFAULT_PARAMS = {
 # }
 
 grid_params = {
-    "train": False,
-    "eval": False,
+    # "train": False,
+    # "eval": False,
     "dataset_name":
         {
             "mustang": {"dataset_dir": "/home/data/mustang/grayscale", "epoch": 5, 'vis_epoch': 4,
