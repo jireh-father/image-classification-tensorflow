3ca71c4418d398310e2e8bb610dff0566138882b
igseo <igseo@gmail.com>
2018-02-25 10:19:38 +0900

    test

--- test4.py
+++ test4.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-lr = 1
+lr = 0.00008
 
 x = tf.placeholder(tf.float32, [1, 2])
 y = tf.placeholder(tf.float32, [1, 1])
@@ -27,7 +27,7 @@ config.gpu_options.allow_growth = True
 sess = tf.Session(config=config)
 sess.run(tf.global_variables_initializer())
 
-step = 8
+step = 10
 
 # dx = np.array([[3, 2], [4, 3], [5, 4]])
 # dy = np.array([[6], [8], [10]])
@@ -36,31 +36,51 @@ step = 8
 #     _, loss_result = sess.run([train, loss], feed_dict={x: dx, y: dy})
 #     print(loss_result)
 
-# dx = np.array([[30, 20]])
+dx = np.array([[30, 20]])
+dy = np.array([[6]])
+
+# dx = np.array([[30.1, 30.]])
 # dy = np.array([[6]])
 
-dx = np.array([[.30, .20]])
-dy = np.array([[6]])
+# dx = np.array([[.30, .20]])
+# dy = np.array([[6]])
 xx = []
 yy = []
 zz = []
-import math
 for i in range(step):
     _, loss_result, weights, gradients1= sess.run([train, loss, w, grad1], feed_dict={x: dx, y: dy})
-    print(loss_result)
-    print(weights)
-    print(gradients1)
-    print(gradients1.var())
+    print("=======================================================================")
+    print("# step %d" % i)
+    print("loss", loss_result)
+    print("weights", weights)
+    print("grad", gradients1)
+    print("gard variance", gradients1.var())
     zz.append(loss_result)
     xx.append(weights[0][0][0])
     yy.append(weights[0][1][0])
+    print("w1 update", weights[0][0][0], -(gradients1[0][0] * lr))
+    print("w2 update", weights[0][1][0], -(gradients1[1][0] * lr))
     # Axes3D.plot_trisurf(gradients1[0][0], gradients1[1][0], loss_result)
     # print(gradients2)
     # print(gradients2.var())
 
 fig = plt.figure()
 ax = fig.gca(projection='3d')
-ax.plot(xx, yy, zz)#, c=['red', 'blue', 'green', 'yellow', 'gray', 'black', 'cyan', 'magenta'])
-# ax.plot_trisurf(xx, yy, zz, linewidth=20, antialiased=True)
-
+c=['red', 'blue', 'green', 'yellow', 'gray', 'black', 'cyan', 'magenta','orange','purple']
+# xx = np.array(xx)
+# yy = np.array(yy)
+# zz = np.array(zz)
+# xx = (xx - xx.min()) / (xx.max() - xx.min())
+# yy = (yy - yy.min()) / (yy.max() - yy.min())
+# zz = (zz - zz.min()) / (zz.max() - zz.min())
+print(xx)
+print(yy)
+print(zz)
+for i in range(len(xx) - 1):
+    ax.plot([xx[i], xx[i+1]], [yy[i], yy[i+1]], [zz[i], zz[i+1]], c=c[i])#, )
+# ax.scatter(xx, yy, zz)
+# ax.plot([1,2],[1,2],[1,2])
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
 plt.show()
